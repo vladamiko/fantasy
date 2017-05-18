7e3a8f2607ea6e48b1bf1c4a0f89115ebba093d5
vladamiko <vladamiko@gmail.com>
2017-05-19 00:53:12 +0300

    added start info

--- app/battle.js
+++ app/battle.js
@@ -8,31 +8,49 @@ function Battle (kingdomName, enemyName) {
             enemyName = this.enemy.name,
             count = 1;
 
+        createStartInfo(kingdomName, this.kingdom.elf.health, enemyName, this.enemy.health);
+
         while (this.isAlive()) {
             let kingdomPower = this.kingdom.attack(),
-                kingdomHealth = this.kingdom.elf.health;
+                kingdomHealth = this.kingdom.elf.health,
+                paramsAttackElement = {
+                    name: kingdomName,
+                    targetName: enemyName,
+                    power: kingdomPower,
+                    className: 'kingdom-attack',
+                    count: count
+                };
 
             this.enemy.defend(kingdomPower);
-            doDeferredTimeout(kingdomName, kingdomPower, kingdomHealth, 'kingdom-attack', enemyName, this.enemy.health, count);
+            paramsAttackElement.targetHealth = this.enemy.health;
+            doDeferredTimeout(paramsAttackElement);
             count++;
 
             if (this.enemy.isAlive()) {
                 let enemyPower = this.enemy.attack(),
-                    enemyHealth = this.enemy.health;
+                    enemyHealth = this.enemy.health,
+                    paramsAttackElement = {
+                    name: enemyName,
+                    targetName: kingdomName,
+                    power: enemyPower,
+                    className: 'enemy-attack',
+                    count: count
+                };
 
                 this.kingdom.defend(enemyPower);
-                doDeferredTimeout(enemyName, enemyPower, enemyHealth, 'enemy-attack', kingdomName, this.kingdom.elf.health, count);
+                paramsAttackElement.targetHealth = this.kingdom.elf.health;
+                doDeferredTimeout(paramsAttackElement);
                 count++;
             }
         }
 
         if (this.enemy.isAlive()) {
             setTimeout(function() {
-                createWinnerElement(enemyName);
+                createWinnerElement(enemyName, 'winner2');
             }, count*750);
         } else {
             setTimeout(function() {
-                createWinnerElement(kingdomName);
+                createWinnerElement(kingdomName, 'winner1');
             }, count*750);
         }
     };
--- app/utils.js
+++ app/utils.js
@@ -1,30 +1,38 @@
-function createAttackElement (name, power, health, className, targetName, targetHealth) {
+function createStartInfo (kingdomName, kingdomHealth, enemyName, enemyHealth) {
+    let divStartInfo = document.createElement('div'),
+        maincontent = document.getElementById('main-content');
+    divStartInfo.innerHTML = `<p>${kingdomName} (${kingdomHealth}hp) vs. ${enemyName} (${enemyHealth}hp)!</p>`;
+    maincontent.appendChild(divStartInfo);
+    divStartInfo.classList.add('start-info');
+}
+
+function createAttackElement (paramsAttackElement) {
     let divPower = document.createElement('div'),
         divHealth = document.createElement('div'),
         maincontent = document.getElementById('main-content');
 
-    divPower.innerHTML = `<p>The ${name} attacks with the power ${power}!</p>`;
-    divHealth.innerHTML = `The ${targetName} health became ${targetHealth}.`;
+    divPower.innerHTML = `<p>The ${paramsAttackElement.name} attacks with the power ${paramsAttackElement.power}!</p>`;
+    divHealth.innerHTML = `The ${paramsAttackElement.targetName} health became ${paramsAttackElement.targetHealth}.`;
     maincontent.appendChild(divPower);
     maincontent.appendChild(divHealth);
 
-    divPower.classList.add(className);
+    divPower.classList.add(paramsAttackElement.className);
     divHealth.classList.add('info');
 }
 
-function doDeferredTimeout (name, power, health, className, targetName, targetHealth, i) {
+function doDeferredTimeout (paramsAttackElement) {
     setTimeout(function () {
-        createAttackElement(name, power, health, className, targetName, targetHealth);
-    }, 750*i);
+        createAttackElement(paramsAttackElement);
+    }, 750*paramsAttackElement.count);
 }
 
-function createWinnerElement (name) {
+function createWinnerElement (name, imageName) {
     let divWinner = document.createElement('div'),
         imgWinner = document.getElementsByTagName('img'),
         maincontent = document.getElementById('main-content');
 
     divWinner.innerHTML = `<p>The ${name} is a winner!</p>
-    <img id="" src="app/img/winner.jpg">`;
+    <img id="" src="app/img/${imageName}.jpg">`;
     maincontent.appendChild(divWinner);
 
     divWinner.classList.add('winner');
